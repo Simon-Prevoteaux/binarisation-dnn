844c3036b11c9a7d9864bca5a8c26b9437da5429
Simon <simon.prevoteaux@gmail.com>
2016-02-11 20:59:38 +0100

    Using new data generation in order to start the learning phase

--- README.md
+++
@@ -1,5 +0,0 @@
-# binarisation-dnn
-Aims to perform binarisation using DNN. We chose to use Crino, which is a ML lib using Theano.
-
-Concerning the dataset, we first planed to use the DIBCO datas, but the images were to much different and we couldn't expect our model to generalize this type of data in the first. Therefore the dataset present on the repository is not used for now.
-Instead, we used some newpapers images on which we performed a binarization using Sauvola's technique. At first, we expect to learn this model, even if the ground truth isn't perfect, it could give us the opportunity to see if our model is able to learn correctly.
--- src/trainDNN.py
+++ src/trainDNN.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from datasets import DataSet
+from training_data import TrainingData
 import sys
 import numpy as np
 import json
@@ -9,13 +10,7 @@ import os
 import pickle 
 from math import sqrt
 
-def main(dataDir):
-	"""
-	training_set=pickle.load(open(dataDir+'/training_set.pck'))
-	x_train=training_set['x_train']
-	y_train=training_set['y_train']
-	"""
-
+def main():
 	needed_params=['learning_params','hidden_geometry','pretraining_geometry','init_weights','save_init_weights','outfolder']
 	config = getConfig()
 	used_config={}
@@ -39,33 +34,31 @@ def main(dataDir):
 		os.mkdir(absoutfolder)
 
 	print('... saving used configuration')
-	json.dump(used_config,open(os.path.join(absoutfolder,"configuration.json"),'wb'),indent=2)
-
+	json.dump(used_config,open(os.path.join(absoutfolder,"dnn_configuration.json"),'wb'),indent=2)
+    
+        x_train, y_train=load_data('dataSauvola')
 
 def getConfig():
     config={}
 
-    dataConfig=loadDataConfig()
-    batch_size=dataConfig['bloc_size']
-    batch_size=batch_size*batch_size
 
     #Learning parameters of the input pretraining
     input_pretraining_params={
             'learning_rate': 10.0,
-            'batch_size' : batch_size,
+            'batch_size' : 10000,
             'epochs' : 50
     }
     #Learning parameters of the output pretraining
     output_pretraining_params={
             'learning_rate': 10.0,
-            'batch_size' : batch_size,
-            'epochs' : 100
+            'batch_size' : 10000,
+            'epochs' : 10
     }
     
     #Learning parameters of the supervised training + pretrainings
     config['learning_params']={
         'learning_rate' : 10.0,
-        'batch_size' : batch_size,
+        'batch_size' : 10000,
         'epochs' : 150,
         'input_pretraining_params' : input_pretraining_params,
         'output_pretraining_params' : output_pretraining_params,
@@ -73,7 +66,7 @@ def getConfig():
     }
     
     #Size of one hidden representation
-    hidden_size = sqrt(batch_size)*2
+    hidden_size = 2*64
     #Geometry of all hidden representations 
     config['hidden_geometry'] = [hidden_size, hidden_size]
 
@@ -98,12 +91,33 @@ def loadDataConfig(name='configuration.json',path='data'):
 	print('Loading used configuration')
 	return json.load(open(os.path.join(path,name),'rb'))
 
+def load_data(dataDir):
+    print 'Loading datasets...'
+    ds_train_samples = DataSet(dataDir+'/train/samples')
+    ds_train_gt = DataSet(dataDir+'/train/groundTruth')
+    print 'Loading data generation parameters...'
+    data = TrainingData(ds_train_samples, ds_train_gt)
+    try:
+        data.load_config_file('config.json')
+    except IOError:
+        data.config['patch_size'] = 100
+        try:
+            data.save_config_file('config.json')
+        except IOError:
+            print 'Cannot save configuration file.'
+    try:
+        genconf = data.load_gen_config('generation_params.pck')
+    except IOError:
+        genconf = data.random_gen_config(1000)
+        try:
+            data.save_gen_config('generation_params.pck', genconf)
+        except IOError:
+            print 'Cannot save generation parameters.'
+    print 'Generating data...'
+    samples_data = data.generate_input_data(genconf)
+    gt_data = data.generate_ground_truth_data(genconf)
+    print samples_data.shape[0], 'training samples were generated.'
+    return samples_data, gt_data
 
 if __name__=='__main__':
-
-	if (len(sys.argv)<2):
-		dataDir='./results'
-	else:
-		dataDir=sys.argv[1:][0]
-
-	main(dataDir)
+	main()
