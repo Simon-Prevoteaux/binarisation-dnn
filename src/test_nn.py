fixed the computation of the validation error

src/test_nn.py
@@ -13,7 +13,7 @@ from crino.network import MultiLayerPerceptron;
 from train_nn import load_data
 from train_nn import normalise
 from math import sqrt
-
+from validation import threshold
 
 # DEFINE PATCH SIZE
 patch_size=20
@@ -24,7 +24,7 @@ print('... loading used configuration')
 config = json.load(open(os.path.join(outfolder,"dnn_configuration.json"),'r'))
 
 #loading test data
-x_test,y_test=load_data('dataSauvola/test/',patch_size=patch_size,nb_random_patchs=1)
+x_test,y_test=load_data('dataSauvola/test/',patch_size=patch_size,nb_random_patchs=5)
 
 x_test = np.asarray(x_test, dtype=theano.config.floatX) # We convert to float32 to 
 y_test = np.asarray(y_test, dtype=theano.config.floatX) # compute on GPUs with CUDA
@@ -44,6 +44,7 @@ nn.setParameters(pickle.load(open('./results/learned_params.pck')))
 for i in xrange(0, nTest):
     image = x_test[i].reshape(sqrt(nFeats),sqrt(nFeats))
     estimated_binarisation = nn.forward(x_test[i:i+1])
+    estimated_binarisation=threshold(estimated_binarisation)
     estimated_image = estimated_binarisation.reshape(sqrt(nFeats),sqrt(nFeats))
     gt = y_test[i].reshape(sqrt(nFeats),sqrt(nFeats))
     

src/validation.py
@@ -61,8 +61,10 @@ def main():
             nn.setParameters(pickle.load(open(weight_folder+'/'+param)))
             for i in xrange(0, nTest):
                 estimated_binarisation = nn.forward(x_test[i:i+1])
-                y_est=treshold(estimated_binarisation)
-                validation_error[k]+=jaccard_index(y_est[0],y_test[i])
+                y_est=threshold(estimated_binarisation)
+                validation_error[k]+=(1-jaccard_index(y_est[0],y_test[i]))
+                #validation_error[k]+=sum(y_test[0]!=y_test)/float(nTest)
+            validation_error[k]=validation_error[k]/float(nTest)
             k+=1
 
         pickle.dump(validation_error,open(os.path.join(absoutfolder,'jaccard_index.pck'),'w'),protocol=-1)
@@ -70,16 +72,16 @@ def main():
         print(validation_error)
         plt.subplot(1,1,1)
         plt.plot(validation_error)
-        plt.title('jaccard index')
+        plt.title('validation error using jaccard index')
         plt.show()
 
 def jaccard_index(y_est,y_true):
     return sum(np.logical_and(y_est,y_true))/float(sum(np.logical_or(y_est,y_true)))
 
-def treshold(nn_output):
-    treshold=0.5
-    nn_output[nn_output<treshold]=0
-    nn_output[nn_output>=treshold]=1
+def threshold(nn_output):
+    threshold=0.5
+    nn_output[nn_output<threshold]=0
+    nn_output[nn_output>=threshold]=1
     return nn_output
 
 if __name__=='__main__':