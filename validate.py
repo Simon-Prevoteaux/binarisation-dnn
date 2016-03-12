0bb1b4c40a3e63471947338b4ee4c58b9b845263
Nathan Lapel <nathan.lapel@insa-rouen.fr>
2016-03-12 16:13:27 +0100

    Added a plot to represent the evolution of the Jaccard distances

--- validate.py
+++ validate.py
@@ -5,6 +5,7 @@ from binarisation.project import Project
 import os.path
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
 
 def jaccard_distance(estimate, ground_truth):
     return 1.0 - np.logical_and(estimate, ground_truth).sum(dtype=np.float_) / np.logical_or(estimate, ground_truth).sum(dtype=np.float_)
@@ -15,21 +16,32 @@ def threshold(estimate):
     out[estimate > threshold_value] = 1
     return out
 
-def validate(model, model_filename, input_data, output_data):
+def plot_validation_results(model_name, epoch_ids, jaccard_distances):
+    plt.figure()
+    plt.title('Model ' + model_name)
+    plt.xlabel('Epoch')
+    plt.ylabel('Jaccard distance')
+    plt.plot(epoch_ids, jaccard_distances)
+
+def validate(model, model_filename, model_name, input_data, output_data):
     model_file = open(model_filename, 'rb')
+    epoch_ids = []
+    jaccard_distances = []
     # For each logged set of weights until we reach EOF
     while True:
         try:
-            epoch_id = model.load_weights(model_file)
+            epoch_ids.append(model.load_weights(model_file))
         except EOFError:
             break
-        print "Epoch " + str(epoch_id)
+        print 'Epoch ' + str(epoch_ids[-1])
         # Compute the estimate
         estimate = model.apply(input_data)
         # Compute Jaccard distance between the estimate and the ground truth
         binarised_estimate = threshold(estimate)
-        print "Jaccard distance: " + str(jaccard_distance(binarised_estimate, output_data))
+        jaccard_distances.append(jaccard_distance(binarised_estimate, output_data))
+        print 'Jaccard distance: ' + str(jaccard_distances[-1])
     model_file.close()
+    plot_validation_results(model_name, epoch_ids, jaccard_distances)
 
 def main():
     # Parse command line args
@@ -43,7 +55,7 @@ def main():
         models_dir = '.'
     # Check that the models directory exists
     if not os.path.exists(models_dir):
-        raise IOError("The models directory does not exist.")
+        raise IOError('The models directory does not exist.')
     # Load project
     project = Project(project_file)
     # Generate validation data
@@ -51,15 +63,17 @@ def main():
     validation_genconfig = project.get_validation_genconfig()
     input_data = validation_dataset.generate_input_data(validation_genconfig)
     output_data = validation_dataset.generate_ground_truth_data(validation_genconfig)
-    print str(input_data.shape[0]) + " validation samples were generated."
-    print "Each sample contains " + str(input_data.shape[1]) + " pixels."
+    print str(input_data.shape[0]) + ' validation samples were generated.'
+    print 'Each sample contains ' + str(input_data.shape[1]) + ' pixels.'
     # Validate each model
     for i in range(project.get_models_count()):
         model = project.get_model(i)
-        model_filename = project.get_model_name(i)
-        print "Validating model " + model_filename + "..."
-        model_filename = os.path.join(models_dir, model_filename + ".model")
-        validate(model, model_filename, input_data, output_data)
+        model_name = project.get_model_name(i)
+        print 'Validating model ' + model_name + '...'
+        model_filename = os.path.join(models_dir, model_name + '.model')
+        validate(model, model_filename, model_name, input_data, output_data)
+    # Show all plots
+    plt.show()
 
 if __name__ == '__main__':
     main()
