515b0201700941bb7b3f95f59790331277c8efb2
Nathan Lapel <nathan.lapel@insa-rouen.fr>
2016-04-18 21:49:59 +0200

    Cleaned up the image binarization code

--- binarize_image.py
+++ binarize_image.py
@@ -1,142 +1,79 @@
 #!/usr/bin/env python2
-# -*- coding: utf-8 -*-
 
 from binarisation.project import Project
-import os.path
+from binarisation.datasets import DataSet
+from binarisation.training_data import TrainingData
 import sys
+import os
 import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
 import theano
+import matplotlib.pyplot as plt
 
-from cv2 import imshow,waitKey
-
-def get_modele_from_name(project, model_name):
-	
+def get_model_from_name(project, model_name):
     for i in range(project.get_models_count()):
-    	if (project.get_model_name(i) == model_name):
-    		print 'The model '+model_name+' has been correctly found' 
-    		return project.get_model(i)
-	return None     
+        if project.get_model_name(i) == model_name:
+            return project.get_model(i)
+    raise LookupError('Could not find model with name ' + model_name)
 
-def load_best_epoch(model, model_name, model_filename,best_epoch_number):
-    model_file = open(model_filename, 'rb')
+def load_weights(model, model_name, model_filename, best_epoch_number):
+    # Unfortunately, the model system is designed in a way wich requires us to iterate through all the epochs before finding the right one
+    # TODO: Fix this issue
+    with open(model_filename, 'rb') as model_file:
+        while True:
+            try:
+                k = model.load_weights(model_file)
+            except EOFError:
+                break
+            if k == best_epoch_number:
+                return model.load_weights(model_file)
+    raise IOError('Could not find epoch number ' + str(best_epoch_number))
 
-    k=0
-    while k!=best_epoch_number:
-        try:
-            k=model.load_weights(model_file)
-        except EOFError:
-            break
-    print 'Successfully found the best weights for '+model_name
-    model_file.close()
-
-def exhaustive_gen_config(test_image,patches_padding, psize):
-    """
-    Creates a generation configuration containing the whole images.
-    Args:
-        patches_padding (numpy.ndarray): (x, y) padding between each patch of the image.
-    Returns:
-        Generation configuration.
-    """
-    (width, height) = test_image.size
-    curlist = []
-    for y in range(0, height - psize, patches_padding[1]):
-        for x in range(0, width - psize, patches_padding[0]):
-            curlist.append([x, y])
-        gen_config = np.array(curlist, np.int16)
-    return gen_config
-
-def binarize(psize, gen_config, model,test_image):
-    pcount = gen_config.shape[0]
-    data = np.empty([pcount, psize * psize], theano.config.floatX)
-    i = 0
-
-    data = np.empty([pcount, psize * psize], theano.config.floatX)
-
-    (width, height) = test_image.size
-
-    image=np.zeros([height,width])
-    print pcount
-
-    print gen_config
-
-    #for j in range(0,pcount-1):
-    for j in range(10000,30000):
-        data[j,:] = test_image.crop((gen_config[j][0], gen_config[j][1], gen_config[j][0] + psize, gen_config[j][1]+ psize)).getdata()
-        temp=data[j,:]/255.0
-        #a normaliser !!!!!!!
-        estimate = model.apply(temp)
-        binarised_estimate = threshold(estimate)
-
-
-
-        # to have the display patch per patch 
-        # /
-        '''
-        plt.subplot(2,2,1)
-        plt.title('output of the network')
-        plt.imshow(estimate.reshape((psize,psize)), interpolation='nearest', cmap=plt.get_cmap('gray'), vmin=0.0, vmax=1.0)
-        plt.subplot(2,2,2)
-        plt.title('original image')
-        plt.imshow(temp.reshape((psize,psize)), interpolation='nearest', cmap=plt.get_cmap('gray'), vmin=0.0, vmax=1.0)
-        plt.subplot(2,2,3)
-        plt.title('binarized image')
-        plt.imshow(binarised_estimate.reshape((psize,psize)), interpolation='nearest', cmap=plt.get_cmap('gray'), vmin=0.0, vmax=1.0)
-        plt.show()
-        '''
-        # \
-        #
-        
-
-        image[gen_config[j][1]:gen_config[j][1] + psize,gen_config[j][0]:gen_config[j][0]+ psize] = binarised_estimate.reshape((psize,psize))
-    return image
-
-def threshold(estimate):
-    threshold_value = 0.4
-    out = np.zeros(estimate.shape)
-    out[estimate > threshold_value] = 1
-    return out
+def binarize(patches, patch_size, gen_config, model, image_size):
+    print 'Binarizing image...'
+    binpatches = model.apply(patches)
+    print 'Reconstructing image...'
+    data = np.empty((image_size[1], image_size[0]))
+    for i in range(len(gen_config)):
+        x = gen_config[i][0]
+        y = gen_config[i][1]
+        data[y : y + patch_size, x : x + patch_size] = binpatches[i].reshape(patch_size, patch_size)
+    return data
 
 def main():
     # Parse command line args
     if len(sys.argv) < 2:
-        print 'USAGE: binarize_image.py project_file.json model_name best_epoch_number test_image_file [models_dir]'
+        print 'USAGE: binarize_image.py project_file model_name best_epoch_number test_image_file [models_dir]'
         sys.exit(1)
     project_file = sys.argv[1]
-
     model_name = sys.argv[2]
-
-    best_epoch_number = sys.argv[3]
-
-    if len(sys.argv) >= 5:
-        test_image_file = sys.argv[4]
-    else:
-    	test_image_file = './dataSauvola/test/samples/29/FRAD076_JPL3_81_0001.tif'
-
+    best_epoch_number = int(sys.argv[3])
+    test_image_file = sys.argv[4]
     if len(sys.argv) >= 6:
         models_dir = sys.argv[5]
     else:
         models_dir = '.'
 
-	project = Project(project_file)
-
-	model_filename = os.path.join(models_dir, model_name + '.model')
-	model=get_modele_from_name(project,model_name)
-
-    load_best_epoch(model, model_name,model_filename,best_epoch_number)
-
-    test_image=Image.open(test_image_file).convert('F')
-
-    gen_config=exhaustive_gen_config(test_image,[10,10],10)
-
-
-    binarize_image=binarize(10,gen_config,model,test_image)
-
-    print np.unique(binarize_image)
-    print binarize_image.shape 
-    im = Image.fromarray(np.uint8(binarize_image*255))
-    im.show()
+    # Load the project file and the model
+    project = Project(project_file)
+    model_filename = os.path.join(models_dir, model_name + '.model')
+    model = get_model_from_name(project, model_name)
+    # Load the weight corresponding to the right epoch
+    load_weights(model, model_name, model_filename, best_epoch_number)
+    # Load the image
+    dataset = DataSet('.')
+    dataset.imagespaths[test_image_file] = test_image_file
+    data = TrainingData(dataset, dataset)
+    patch_size = project.get_training_dataset().config['patch_size']
+    data.set_config({'patch_size': patch_size})
+    genconfig = data.exhaustive_gen_config([patch_size, patch_size])
+    patches = data.generate_input_data(genconfig)
+    # Binarize the image
+    binarized_image = binarize(patches, patch_size, genconfig[test_image_file], model, dataset.open_image(test_image_file).size)
+    # Draw the image
+    print(binarized_image.shape)
+    plt.imshow(binarized_image, cmap='gray')
+    plt.show()
 
 if __name__ == '__main__':
     main()
+
